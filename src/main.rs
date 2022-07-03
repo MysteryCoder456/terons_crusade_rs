1ee283f637448a76618bae34aadd2121cba9ee8e
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-07-03 11:24:03 +0400

    basic bevy stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Teron's Crusade".to_owned(),
+            width: 1080.,
+            height: 720.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    // Add camera bundles
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // Green rectangle
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::GREEN,
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0., 0., 0.),
+            scale: Vec3::new(50., 50., 1.),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
