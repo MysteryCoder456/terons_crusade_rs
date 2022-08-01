b6d990cfd6c66c7b82e6a3ead20dd9048a694580
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-08-01 23:04:04 +0400

    switched from json -> binary serialization

--- src/save_data.rs
+++ src/save_data.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::components::SpawnBlock;
 
-const SAVE_DATA_PATH: &str = "save_data";
+const SAVE_DATA_PATH: &str = "world_saves";
 
 pub struct SaveDataPlugin;
 
@@ -129,19 +129,19 @@ fn save_data_setup_system(mut commands: Commands) {
     }
 
     // Load world data
-    let world_data_path = save_data_path.join("world_data.json");
+    // TODO: Add multiple save files in the future
+    let world_data_path = save_data_path.join("world0.save");
 
     let world_data = if world_data_path.exists() {
         // Load world data from save file
-        let json_text =
-            std::fs::read_to_string(world_data_path).expect("Error reading world save data!");
-
-        serde_json::from_str(&json_text).expect("Error parsing world data!")
+        let world_data_bytes =
+            std::fs::read(world_data_path).expect("Error reading world save data!");
+        bincode::deserialize(&world_data_bytes).expect("Error deserializing world data!")
     } else {
         // Generate and save default world
         let default_world_data = WorldSaveData::default();
         let default_world_serialized =
-            serde_json::to_string(&default_world_data).expect("Error serializing world data!");
+            bincode::serialize(&default_world_data).expect("Error serializing world data!");
         std::fs::write(world_data_path, default_world_serialized)
             .expect("Error writing world data!");
 
