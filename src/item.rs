df66eb7d102b8e86fa5ececbd7fe5bdabb924eca
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-08-02 11:54:04 +0400

    added item data

--- src/item.rs
+++ src/item.rs
@@ -1,11 +1,24 @@
 use bevy::{prelude::*, utils::HashMap};
+use serde::Deserialize;
 // use bevy_rapier2d::prelude::*;
 
 use crate::{components::SpawnItem, SPRITE_SCALE};
 
 const ITEMS_DIR: &str = "assets/items";
 
-type ItemSprites = HashMap<String, Handle<Image>>;
+type Items = HashMap<String, ItemData>;
+
+#[derive(Deserialize)]
+struct ItemData {
+    category: String,
+    subcategory: Option<String>,
+    stack_size: u32,
+    damage: Option<f32>,
+    knockback: Option<f32>,
+
+    #[serde(skip)]
+    sprite: Handle<Image>,
+}
 
 pub struct ItemPlugin;
 
@@ -17,38 +30,56 @@ impl Plugin for ItemPlugin {
 }
 
 fn item_setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let item_sprites = std::fs::read_dir(ITEMS_DIR)
+    let items = std::fs::read_dir(ITEMS_DIR)
         .unwrap()
         .filter_map(|dir| match dir {
             Ok(item_dir) => {
+                if !item_dir.path().is_dir() {
+                    return None;
+                }
+
                 let os_str = item_dir.file_name();
                 let item_name = os_str.to_str().unwrap();
 
                 let sprite_path = format!("items/{0}/{0}.png", item_name);
-                let img_handle: Handle<Image> = asset_server.load(&sprite_path);
+                let data_path = format!("assets/items/{0}/{0}.json", item_name);
 
-                Some((item_name.to_owned(), img_handle))
+                let img_handle: Handle<Image> = asset_server.load(&sprite_path);
+                let item_data_str = std::fs::read_to_string(data_path).unwrap();
+
+                let data = serde_json::from_str::<ItemData>(&item_data_str);
+
+                if data.is_err() {
+                    eprintln!("Error loading item data for {}", item_name);
+                    return None;
+                }
+
+                let data = ItemData {
+                    sprite: img_handle,
+                    ..data.unwrap()
+                };
+
+                Some((item_name.to_owned(), data))
             }
             Err(e) => {
                 eprintln!("Error while loading item: {}", e);
                 None
             }
         })
-        .collect::<ItemSprites>();
+        .collect::<Items>();
 
-    println!("{:?}", item_sprites);
-    commands.insert_resource(item_sprites);
+    commands.insert_resource(items);
 }
 
 fn item_spawn_system(
     mut commands: Commands,
-    item_sprites: Res<ItemSprites>,
+    items: Res<Items>,
     query: Query<(Entity, &SpawnItem)>,
 ) {
     for (entity, spawn_item) in query.iter() {
-        if let Some(item_img) = item_sprites.get(&spawn_item.item_name) {
+        if let Some(item_data) = items.get(&spawn_item.item_name) {
             commands.spawn_bundle(SpriteBundle {
-                texture: item_img.clone(),
+                texture: item_data.sprite.clone(),
                 transform: Transform {
                     translation: Vec3::new(spawn_item.position.x, spawn_item.position.y, 0.0),
                     scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, SPRITE_SCALE),
